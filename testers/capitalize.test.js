23441d2a46ddb168b53b15e9daa46c104679aef8
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-21 19:29:44 +0300

    Added test case for integer input

--- testers/capitalize.test.js
+++ testers/capitalize.test.js
@@ -4,4 +4,7 @@ describe('Testing the capitalize function', () => {
   test('Capitalizing "excellence" should return "Excellence"', () => {
     expect(capitalize('excellence')).toMatch('Excellence');
   });
+  test('Providing a number should throw an error', () => {
+    expect(() => capitalize(1)).toThrow('Invalid string!');
+  });
 });
